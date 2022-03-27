af907d41737699864d97179d3eeff093a8896185
SiamSarker <msarker192014@bscse.uiu.ac.bd>
2022-03-27 14:47:35 +0600

    Changes

--- blogentry.php
+++ blogentry.php
@@ -57,6 +57,7 @@ if (!isset($_SESSION['username'])) {
                         }
 
 
+                        /* hello */
                     #ptable{
                         width: 100%;
                         border: 1px solid blue;
